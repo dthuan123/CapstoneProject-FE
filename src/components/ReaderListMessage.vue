a66707a9d119046c5feb0b8151ff8edb99dd34f2
Tran Viet Anh <anhtvhe130172@fpt.edu.vn>
2021-06-23 16:18:46 +0700

    list message2

--- src/components/ReaderListMessage.vue
+++ src/components/ReaderListMessage.vue
@@ -3,41 +3,24 @@
         {{userId}}
         <div v-for="report in messageListByUser" :key="report.id" v-bind:userId="userId">
             {{ report.reportId}} {{ report.reportContent}} {{ report.responseDate}}
-        </div>
-        <reader-message-list
-            v-bind:userId="userId"
-        >
-        </reader-message-list>
-        <reader-data-table
-            v-bind:columnDefs="columnDefs"
-            v-bind:data="data"
-            v-bind:paging="paging"
-            v-bind:url="url"
-        >
-        </reader-data-table>        
+        </div>        
   </div>
 </template>
 
 <script>
-import axios from"axios"
-export default {
-   
-        
-        props: ["report"],
-    
-    name: "ReaderListMessage",
+import axios from "axios"
+export default {         
+        props: ["report"],   
+        name: "ReaderListMessage",
     data() {
         return {
             messageListByUser: [],
             userId: this.$store.state.userId,
-            columnDefs: null,
-            data: null,
-            paging: null,
-            url: null
+            data: null,           
         }
     },
     beforeMount() {
-        this.get();
+        this.getMessageByUser();
     },
     methods: {
         getMessageByUser() {
