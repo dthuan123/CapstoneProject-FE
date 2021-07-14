0220ca2b7d9c5b856c68968629eae14db728cbba
vanhub03 <vietanh.dao99@gmail.com>
2021-07-15 03:23:19 +0700

    get time comment

--- src/components/Bookinformation.vue
+++ src/components/Bookinformation.vue
@@ -349,9 +349,14 @@ export default {
                 });
         },
         currentDate() {
-            const current = new Date();
-            const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
-            return date;
+            // const current = new Date();
+            // const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+            // return date;
+            const today = new Date();
+                    const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+                    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+                    const dateTime = date +' '+ time;
+            return dateTime;
         },
         sendReport() {
             // console.log(this.user);
